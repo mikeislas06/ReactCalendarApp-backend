ba5b305826b35d99bd4248d28ca5af5a55fcbb17
Miguel Islas <isgromiguel@gmail.com>
2022-01-13 23:20:05 -0600

    cors back to original

--- index.js
+++ index.js
@@ -14,22 +14,7 @@ const app = express();
 dbConnection();
 
 // CORS
-app.use(
-  cors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  })
-);
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
-//   );
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
+app.use(cors());
 
 // Morgan
 app.use(morgan('combined'));
